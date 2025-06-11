4751d31c4b5475d0337eff7d229d3acbab9f93a7
ErickDev1218 <erickpdm879@gmail.com>
2025-06-10 23:41:38 -0300

    subindo numberGeneratorApp

---
+++ NumberGeneratorApp/app/src/main/java/com/example/numbergeneratorapp/NumberUiState.kt
@@ -0,0 +1,14 @@
+package com.example.numbergeneratorapp
+
+
+
+
+
+sealed class NumberUiState {
+    object Idle : NumberUiState()
+    object Loading : NumberUiState()
+    data class Success(val number : Int) : NumberUiState()
+    data class Error(val message : String) : NumberUiState()
+
+
+}
